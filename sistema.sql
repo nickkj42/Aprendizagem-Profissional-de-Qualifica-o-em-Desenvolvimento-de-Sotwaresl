fb76c407148b5f85b3733a5d6e6038744d5e3626
nic <misterscraft13@gmail.com>
2024-07-31 15:20:32 -0300

    Update sistema.sql

--- sistema.sql
+++ sistema.sql
@@ -13,6 +13,25 @@ add constraint `fk_prod_comp_compra`
    references `bd_caso_estudo_vendas`.`tb_compra`(`compra_cod`)
    on delete no action
    on update no action;
+
+/*Criando index e definindo que um produto é definido por um fornecedor*'
+alter table `bd_caso_estudo_vendas`.`tb_prod`
+add index `fk_prod_forn_indx`(`prod_forn_cod` ASC);
+
+/*Criando chave estrangeira*/
+alter table `bd_caso_estudo_vendas`.`tb_prod`
+
+/*Criando index e definindo que cliente possui muitos telefone*/
+alter table `bd_caso_estudo_vendas`.`tb_cli_tel`
+add index `fk_cli_tel_idx` (`tel_cli_cod` asc);
+
+/*Adicionando chave estrangeira*/
+alter table `bd_caso_estudo_vendas`.`tb_cli_tel`
+add constraint `fk_cli_tel`
+	foreign key  (`tel_cli_cod`)
+    references  `bd_caso_estudo_vendas`.`tb_cli`(`cli_cod`)
+	on delete no action
+    on update no action;
    
    alter table `bd_caso_estudo_vendas`.`tb_prod_comp`
    add constraint `fk_prod_comp_prod`
